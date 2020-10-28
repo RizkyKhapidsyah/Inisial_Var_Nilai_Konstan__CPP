522507e67b12b888d701f628c51bd209a6ec35da
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2020-10-28 10:51:26 +0700

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	int jumlah = 10; // inisialisasi
+	float harga_per_unit = 17.5; // inisialisasi
+
+	
+	cout << "Isi Jumlah = "	<< jumlah << "\n";
+	cout << "Isi harga per per unit = "<< harga_per_unit << "\n";
+
+	_getch();
+	return 0;
+}
